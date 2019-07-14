608c00c8d0bd9c02cdc16f480e2f0624a129f3f0
Martin Rubi <martinrubi@gmail.com>
2019-07-14 13:32:20 -0300

    Ejercicio completado.

---
+++ backend/tests/apiv1/getProducts.test.ts
@@ -0,0 +1,99 @@
+import {get} from '../requests'
+import initializeApp from '../../app'
+
+/**
+ * Endpoint spec:
+ *      https://trello.com/c/Po7FF8IU/2-get-apiv1-products
+ */
+describe('GET /apiv1/products endpoint', () => {
+    let app = undefined
+
+    beforeAll( async (done) => {
+        app = await initializeApp()
+
+        done()
+    })
+
+    afterAll( async (done) => {
+        await app.closeConnection()
+
+        done()
+    })
+
+    test('returns 200', async (done) => {
+        const response = await get(app, '/apiv1/products')
+
+        expect(response.statusCode).toBe(200)
+
+        done()
+    })
+
+    test('returns the available units', async (done) => {
+        const response = await get(app, '/apiv1/products')
+
+        expect(response.body).toEqual({
+            products: expect.arrayContaining([
+                { 
+                    name: 'Tomates',
+                    id: 1,
+                    units: expect.arrayContaining([
+                        { id: 1, name: 'unidades' },
+                        { id: 3, name: 'gr' },
+                        { id: 4, name: 'kg' },
+                    ])
+                },
+                { 
+                    name: 'Cebollas',
+                    id: 2,
+                    units: expect.arrayContaining([
+                        { id: 1, name: 'unidades' },
+                        { id: 3, name: 'gr' },
+                        { id: 4, name: 'kg' },
+                    ])
+                },
+                { 
+                    name: 'Lechuga',
+                    id: 3,
+                    units: expect.arrayContaining([
+                        { id: 1, name: 'unidades' },
+                        { id: 3, name: 'gr' },
+                        { id: 4, name: 'kg' },
+                    ])
+                },
+                { 
+                    name: 'Asado',
+                    id: 4,
+                    units: expect.arrayContaining([
+                        { id: 3, name: 'gr' },
+                        { id: 4, name: 'kg' }
+                    ])
+                },
+                { 
+                    name: 'Milanesas',
+                    id: 5,
+                    units: expect.arrayContaining([
+                        { id: 1, name: 'unidades' },
+                        { id: 3, name: 'gr' },
+                        { id: 4, name: 'kg' },
+                    ])
+                },
+                { 
+                    name: 'Huevos',
+                    id: 6,
+                    units: expect.arrayContaining([
+                        { id: 2, name: 'docena' },
+                    ])
+                },
+                {
+                    name: 'Ajo',
+                    id: 7,
+                    units: expect.arrayContaining([
+                        { id: 1, name: 'unidades' }
+                    ])
+                },
+            ])
+        })
+
+        done()
+    })
+})
